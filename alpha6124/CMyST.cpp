88cf01cc14738f38d2d8b846a23f66a77dc2c635
gbm618 <gbm618@gmail.com>
2018-01-16 23:04:51 +0800

    tdx

--- alpha6124/CMyST.cpp
+++ alpha6124/CMyST.cpp
@@ -28,7 +28,7 @@ int CMyST::testTDX(char *fname)
 	for (m=0;;m++) {
 		n = fread(uif[0].buf, 4, 8, fp);
 		if (n < 1)break;
-		printf("%d %.3f %.3f %.3f %.3f %.4f %.4f\n", uif[0].i,uif[1].i*.01,uif[2].i*.01,uif[3].i*.01,uif[4].i*.01,uif[5].i*.0001,uif[6].i*.0001);
+		printf("%d %.3f %.3f %.3f %.3f %.4f %.4f\n", uif[0].i,uif[1].i*.01,uif[2].i*.01,uif[3].i*.01,uif[4].i*.01,uif[6].i*.0001*.0001,uif[5].f*.0001*.0001);
 	}
 
 	fclose(fp);
@@ -50,8 +50,8 @@ int CMyST::testTDX1(char* fname)
 	int m;
 
 	printf("test\n");
-	//fp = fopen("r:\\sz000725.day", "rb");
-	fp = fopen("r:\\sz399006.lc1", "rb");
+	fp = fopen("r:\\sz000725.day", "rb");
+	//fp = fopen("r:\\sz399006.lc1", "rb");
 	if (fp == NULL) {
 		printf("open file error\nexit\n");
 		return -1;
